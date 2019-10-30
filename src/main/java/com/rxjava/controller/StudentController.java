6b3a24ff694c607e6e787f3e0d45b8afa8b706f0
yramirec <yordan.ramirez@outlook.com>
2019-10-30 10:39:27 -0500

    Iniciando Git-Flow

---
+++ src/main/java/com/rxjava/controller/StudentController.java
@@ -0,0 +1,81 @@
+package com.rxjava.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rxjava.model.Student;
+import com.rxjava.service.StudentService;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	/*
+	 * Method by List Student
+	 */
+	@GetMapping()	
+	@ResponseStatus(HttpStatus.OK)
+	public Flowable<Student> findAll() {
+		return studentService.findAll();
+	}
+		
+	/*
+	 * Method by List for id of Student
+	 */
+	@GetMapping("{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Maybe<Student> findById(@PathVariable String idStudent) {
+		return studentService.findById(idStudent);
+	}
+
+	/*
+	 * Method by Create Student
+	 */
+	@PostMapping(
+			value = "/students", 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	
+	public Single<Student> save(Student student) {
+		return studentService.save(student);
+	}
+		
+	/*
+	 * Method by Update Student
+	 */
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	
+	public Single<Student> update(Student student) {
+		return studentService.save(student);
+	}
+
+	/*
+	 * Method by Delete Student
+	 */
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Completable delete(String idStudent) {
+		return studentService.delete(idStudent);
+	}
+	
+	
+	
+}
